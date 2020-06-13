e80e158b88954b83c22fef8135ae9a6c0baa5f32
阿部直也 <n.abe@gemcook.com>
2020-06-13 12:02:01 +0900

    Added User model and JSON server.

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hi");
+import {User} from "./models/User";
+
+const user = new User({name: "myname", age: 20});
+
+user.set({name: "new name", age: 9999});
+
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
+});
+
+user.trigger("change");
