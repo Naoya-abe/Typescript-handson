aa6e3dd3db188825f9fa4182ad7e90fb20031c1f
阿部直也 <n.abe@gemcook.com>
2020-06-16 08:39:13 +0900

    Composition vs Inheritance...Again!

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,4 @@
+import {AxiosResponse} from "axios";
 import {Eventing} from "./Eventing";
 import {Sync} from "./Sync";
 import {Attributes} from "./Attributes";
@@ -7,10 +8,49 @@ export interface UserProps {
   name?: string;
   age?: number;
 }
-
 const rootUrl = "http://localhost:3000/users";
 
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs : UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+  get on() {
+    return this.events.on;
+  }
+  get trriger() {
+    return this.events.trigger;
+  }
+
+  set(update : UserProps): void {
+    this.attributes.set(update);
+    this.trriger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response : AxiosResponse) : void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll()).then((response : AxiosResponse) : void => {
+      this.trriger("save");
+    }).catch(() => {
+      this.trriger("error");
+    });
+  }
 }
