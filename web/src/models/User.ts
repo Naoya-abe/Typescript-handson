75ab83f7dfc22320a6003695d31770aff5467333
阿部直也 <n.abe@gemcook.com>
2020-06-17 08:53:03 +0900

    A Class Method for Collections

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,8 @@
-import {AxiosResponse} from "axios";
-import {Eventing} from "./Eventing";
-import {Sync} from "./Sync";
+import {Model} from "./Model";
 import {Attributes} from "./Attributes";
+import {Eventing} from "./Eventing";
+import {ApiSync} from "./ApiSync";
+import {Collection} from "./Collection";
 
 export interface UserProps {
   id?: number;
@@ -10,47 +11,12 @@ export interface UserProps {
 }
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs : UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
+export class User extends Model<UserProps> {
+  static buildUser(attrs : UserProps): User {
+    return new User(new Attributes<UserProps>(attrs), new Eventing(), new ApiSync<UserProps>(rootUrl));
   }
 
-  get get() {
-    return this.attributes.get;
-  }
-  get on() {
-    return this.events.on;
-  }
-  get trriger() {
-    return this.events.trigger;
-  }
-
-  set(update : UserProps): void {
-    this.attributes.set(update);
-    this.trriger("change");
-  }
-
-  fetch(): void {
-    const id = this.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-
-    this.sync.fetch(id).then((response : AxiosResponse) : void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll()).then((response : AxiosResponse) : void => {
-      this.trriger("save");
-    }).catch(() => {
-      this.trriger("error");
-    });
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json : UserProps) => User.buildUser(json));
   }
 }
