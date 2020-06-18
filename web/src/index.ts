1074855d553f0f5aceacfd7e41d1d896886d3524
阿部直也 <n.abe@gemcook.com>
2020-06-19 08:38:01 +0900

    Testing Region Mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,16 @@
-import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import {User} from "./models/User";
+import {UserEdit} from "./views/UserEdit";
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
+const user = User.buildUser({name: "NAME", age: 20});
 
-const userForm = new UserForm(document.getElementById('root'), user);
+const root = document.getElementById("root");
 
-userForm.render();
+if (root) {
+  const userEdit = new UserEdit(root, user);
+
+  userEdit.render();
+
+  console.log(userEdit);
+} else {
+  throw new Error("Root element not found");
+}
